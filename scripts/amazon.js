fc145f37f25a6c585f9ec66eeb7554cfe649eb9f
Kaveh00Gholizadeh00Kapurchali <Sobhan.Gholizadeh.K@gmail.com>
2026-09-19 15:04:33 +0330

    added cart.js for handling cart, added add to cart functionality

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,7 +47,8 @@ products.forEach((product) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary js-add-to-cart-button">
+            <button class="add-to-cart-button button-primary js-add-to-cart-button"
+            data-product-id = "${product.id}">
                 Add to Cart
             </button>
             </div>
@@ -58,7 +59,28 @@ products.forEach((product) => {
 document.querySelector('.js-product-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
-    button.addEventListener('click', () =>{
-        console.log('added product')
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+
+        let matchingItem;
+
+      
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item;
+            } 
+        }); 
+
+      
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            cart.push({
+                productId: productId,
+                quantity: 1 
+            }); 
+        } 
+
+        console.log(cart);
     });
 });
