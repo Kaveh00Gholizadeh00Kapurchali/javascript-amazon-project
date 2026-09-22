2171657a0aff354d83ee77cab848038025fb811e
Kaveh00Gholizadeh00Kapurchali <Sobhan.Gholizadeh.K@gmail.com>
2026-09-22 11:43:48 +0330

    creater checkout js file and added functionality
    deleted the html code for js file to inject it

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,22 @@
- export const cart = []; 
+ export const cart = [
+    {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2
+    },
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity:1
+    }
+ ]; 
 
 
- export function addToCart(productID){
+ export function addToCart(productId){
      
          let matchingItem;
  
        
          cart.forEach((item) => {
-             if (productID === item.productId) {
+             if (productId === item.productId) {
                  matchingItem = item;
              } 
          }); 
@@ -17,7 +26,7 @@
              matchingItem.quantity += 1;
          } else {
              cart.push({
-                 productId: productID,
+                 productId: productId,
                  quantity: 1 
              }); 
          }
